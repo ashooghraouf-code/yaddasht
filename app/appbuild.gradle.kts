6f1d3099c4e6215dc52e2fd45fa6642b436d9190
ashooghraouf-code <ashoogh.raouf@gmail.com>
2026-08-19 13:45:50 +0330

    appbuild.gradle.kts

--- app/appbuild.gradle.kts
+++ app/appbuild.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.biometric:biometric:1.1.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.5")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
