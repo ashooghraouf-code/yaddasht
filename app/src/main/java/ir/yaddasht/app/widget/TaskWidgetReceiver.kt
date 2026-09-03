8954901b92ac9083a63b628e902e2fdf95dd2ac0
ashooghraouf-code <ashoogh.raouf@gmail.com>
2026-09-03 12:33:03 +0330

    TaskWidgetReceiver.kt

--- app/src/main/java/ir/yaddasht/app/widget/TaskWidgetReceiver.kt
+++ app/src/main/java/ir/yaddasht/app/widget/TaskWidgetReceiver.kt
@@ -12,10 +12,8 @@ class TaskWidgetReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == "TOGGLE_TASK") {
             val taskId = intent.getLongExtra("task_id", -1L)
-            
             if (taskId > 0) {
                 CoroutineScope(Dispatchers.IO).launch {
-                    // ✅ فقط یک پارامتر (مطابق TaskDao شما)
                     AppDatabase.get(context).taskDao().markCompleted(taskId)
                     TaskWidget.forceUpdate(context)
                 }
